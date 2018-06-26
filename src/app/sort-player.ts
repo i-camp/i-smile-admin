60f1ae49a0ba0f651916ebc88de232c87ee431ea
yamamotoya <yamamotoya@istyle.co.jp>
2018-06-26 14:28:28 +0900

    集計処理完了 but  テストコードあり

--- src/app/sort-player.ts
+++ src/app/sort-player.ts
@@ -1,199 +1,148 @@
 import { GameProgress } from '@app/game';
+import { chain } from 'lodash';
 
 export class SortPlayer {
-    private sortList: SortedPlayersList;
-    private countList: CountPlayerList;
-    private nameList: PlayerNameList;
-    private testObj: GameProgress;
+    private sortList: SortedPlayersList = {
+        photographer: [
+            {
+                photographerId: null,
+                name: null
+            }
+        ],
+        subject: [
+            {
+                subjectId: null,
+                name: null
+            }
+        ]
+    };
+
+    private nameList: PlayerNameList = {
+        PlayerName: {
+            key: '',
+        }
+    };
+
+    private testObj: GameProgress = {
+        players: [
+            { id: 'p001', name: 'yasu' },
+            { id: 'p002', name: 'tsukamoto' },
+            { id: 'p003', name: 'yoshimatsu' },
+        ],
+        snapEvents: [
+            {
+                photographerId: 'p001',
+                subjectId: 'p002',
+                photoUrl: 'http://test',
+                photoPath: '//test_path',
+                createdAt: new Date('2018-06-19T23:38:52.037Z')
+            },
+            {
+                photographerId: 'p002',
+                subjectId: 'p003',
+                photoUrl: 'http://test',
+                photoPath: '//test_path',
+                createdAt: new Date('2018-06-19T23:38:52.037Z')
+            },
+            {
+                photographerId: 'p003',
+                subjectId: 'p001',
+                photoUrl: 'http://test',
+                photoPath: '//test_path',
+                createdAt: new Date('2018-06-19T23:38:52.037Z')
+            },
+            {
+                photographerId: 'p002',
+                subjectId: 'p003',
+                photoUrl: 'http://test',
+                photoPath: '//test_path',
+                createdAt: new Date('2018-06-19T23:38:52.037Z')
+            },
+            {
+                photographerId: 'p002',
+                subjectId: 'p001',
+                photoUrl: 'http://test',
+                photoPath: '//test_path',
+                createdAt: new Date('2018-06-19T23:38:52.037Z')
+            },
+            {
+                photographerId: 'p003',
+                subjectId: 'p001',
+                photoUrl: 'http://test',
+                photoPath: '//test_path',
+                createdAt: new Date('2018-06-19T23:38:52.037Z')
+            }]
+    };
 
     sortData(data: GameProgress): SortedPlayersList {
 
-        this.testObj = {
-            players: [
-                { id: 'p001', name: 'yasu' },
-                { id: 'p002', name: 'tsukamoto' },
-                { id: 'p003', name: 'yoshimatsu' },
-                { id: 'p002', name: 'tsukamoto' },
-                { id: 'p002', name: 'tsukamoto' },
-                { id: 'p001', name: 'yasu' },
-            ],
-            snapEvents: [
-                {
-                    photographerId: 'p001',
-                    subjectId: 's001',
-                    photoUrl: 'http://test',
-                    photoPath: '//test_path',
-                    createdAt: new Date('2018-06-19T23:38:52.037Z')
-                },
-                {
-                    photographerId: 'p001',
-                    subjectId: 's002',
-                    photoUrl: 'http://test',
-                    photoPath: '//test_path',
-                    createdAt: new Date('2018-06-19T23:38:52.037Z')
-                },
-                {
-                    photographerId: 'p002',
-                    subjectId: 's003',
-                    photoUrl: 'http://test',
-                    photoPath: '//test_path',
-                    createdAt: new Date('2018-06-19T23:38:52.037Z')
-                },
-                {
-                    photographerId: 'p002',
-                    subjectId: 's001',
-                    photoUrl: 'http://test',
-                    photoPath: '//test_path',
-                    createdAt: new Date('2018-06-19T23:38:52.037Z')
-                },
-                {
-                    photographerId: 'p002',
-                    subjectId: 's001',
-                    photoUrl: 'http://test',
-                    photoPath: '//test_path',
-                    createdAt: new Date('2018-06-19T23:38:52.037Z')
-                },
-                {
-                    photographerId: 'p003',
-                    subjectId: 's001',
-                    photoUrl: 'http://test',
-                    photoPath: '//test_path',
-                    createdAt: new Date('2018-06-19T23:38:52.037Z')
-                }]
-        };
+        // 撮影側の集計
+        const photographerScore = chain(this.testObj.snapEvents)
+            .countBy('photographerId')
+            .map((a, b) => ({ photographerId: b, count: a }))
+            .orderBy(['count'], ['desc'])
+            .value();
+
+        console.log('photographerScore');
+        console.log(photographerScore);
+
+        // 被写体側の集計
+        const subjectScore = chain(this.testObj.snapEvents)
+            .countBy('subjectId')
+            .map((a, b) => ({ subjectId: b, count: a }))
+            .orderBy(['count'], ['desc'])
+            .value();
+
+        console.log('subjectScore');
+        console.log(subjectScore);
 
 
-        this.nameList = {
-            string: null
-        };
+        // idとnameの対応リスト
+        const idNameList = chain(this.testObj.players)
+            .filter((obj, i, self) => self.findIndex(obj2 => obj2.id === obj.id) === i)
+            .value();
 
+        console.log('idNameList');
+        console.log(idNameList);
 
-        this.countList = {
-            photographerScore: [{
-                id: null,
-                name: null,
-                count: 0
-            }]
-        };
-        console.log('first this.countList');
-        console.log(this.countList);
-
-        this.sortList = {
-            photographer: [
-                {
-                    photographerId: null,
-                    name: null
-                }
-            ],
-            subject: [
-                {
-                    subjectId: null,
-                    name: null
-                }
-            ]
-        };
-
-        console.log('from this.testObj.players');
-        console.log(this.testObj.players);
-        // playerと名前の対応オブジェクト作成
-        for (const value of this.testObj.players) {
-            if (value.id in this.nameList) {
-            } else {
-                this.nameList[value.id] = value.name;
-            }
-        }
-        console.log('to this.nameList');
-        console.log(this.nameList);
-
-        console.log('from this.testObj.snapEcents');
-        console.log(this.testObj.snapEvents);
-        // playerごとの撮影した枚数をカウントし格納Ï
-        for (const value of this.testObj.snapEvents) {
-            console.log('aaaaaaaaaaaaaaaaa');
-            console.log(this.countList.photographerScore);
-
-            if (this.countList.photographerScore.find(x => x.id === value.photographerId)) {
-                this.countList.photographerScore.map((obj) => {
-                    obj.count++;
-                    console.log('if //////////////////////');
-                    console.log('value.photographerId');
-                    console.log(value.photographerId);
-                    console.log('obj.name + obj.count');
-                    console.log(obj.name + obj.count);
-                    console.log('//////////////////////');
-                });
-            } else {
-                console.log('else //////////////////////');
-                console.log(value.photographerId);
-                console.log(this.nameList[value.photographerId]);
-                console.log('//////////////////////');
-                this.countList.photographerScore.push({
-                    id: value.photographerId,
-                    name: this.nameList[value.photographerId],
-                    count: 1
-                });
-            }
-            console.log(this.countList.photographerScore);
-        }
-        console.log('to this.countList.photographerScore');
-        console.log(this.countList.photographerScore);
-
-        // photographerIdが空白の撮影者を除外する関数
-        function removeSpace(x: any) {
-            return x.id !== '';
-        }
-        const countListRemove = this.countList.photographerScore.filter(removeSpace);
-
-        console.log('to countListremove');
-        console.log(countListRemove);
-
-        // 撮影した枚数の多いプレイヤー順にソート
-        function sortPlayerScore(a: any, b: any) {
-            return b.count - a.count;
-        }
-        const countListSort = countListRemove.sort(sortPlayerScore);
-
-        console.log('to countListremove');
-        console.log(countListSort);
-
-        // ranking.service.tsの private aggregateGameProgress(): Observable<RankingEvent>に合わせて返す
-        // for (const value of this.countList.photographerScore) {
-        for (const value of countListSort) {
-            this.sortList.photographer.push({ photographerId: value.id, name: value.name });
+        // nameと連結しsortListにpush
+        for (const value of photographerScore) {
+            this.sortList.photographer.push({
+                photographerId: value.photographerId,
+                name: idNameList.find(obj => obj.id === value.photographerId).name
+            });
         }
 
-        console.log('to this.sortList.photographer');
-        console.log(this.sortList.photographer);
+        // nameと連結しsortListにpush
+        for (const value of subjectScore) {
+            this.sortList.subject.push({
+                subjectId: value.subjectId,
+                name: idNameList.find(obj => obj.id === value.subjectId).name
+            });
+        }
 
+        this.sortList.photographer = this.sortList.photographer.filter(obj => obj.photographerId !== null);
+        this.sortList.subject = this.sortList.subject.filter(obj => obj.subjectId !== null);
+
+        console.log('sortlist');
+        console.log(this.sortList);
         return this.sortList;
     }
 }
 
 interface SortedPlayersList {
-    photographer: [
-        {
-            photographerId: string;
-            name: string;
-        }
-    ];
-    subject: [
-        {
-            subjectId: string;
-            name: string;
-        }
-    ];
-}
-
-interface CountPlayerList {
-    photographerScore: [
-        {
-            id: string;
-            name: string;
-            count: number;
-        }
-    ];
+    photographer:
+    {
+        photographerId: string;
+        name: string;
+    }[];
+    subject:
+    {
+        subjectId: string;
+        name: string;
+    }[];
 }
 
 interface PlayerNameList {
-    string: string;
+    PlayerName: { key: string };
 }
