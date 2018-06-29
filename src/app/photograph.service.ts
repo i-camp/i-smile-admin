4a43e14f2f6d49116bd8364d15b32b5e68fbde55
tsukamotota <tsukamotota@istyle.co.jp>
2018-06-29 14:14:38 +0900

    デバッグコードの削除とSnapEventsが存在しなかった場合の考慮

--- src/app/photograph.service.ts
+++ src/app/photograph.service.ts
@@ -2,9 +2,7 @@ import {Injectable} from '@angular/core';
 import {AngularFireDatabase} from 'angularfire2/database';
 import {SnapEvent} from '@app/game';
 import {Observable} from 'rxjs/internal/Observable';
-import {GameManagementService} from '@app/game-management.service';
-import {map} from 'rxjs/operators';
-import {interval} from 'rxjs';
+import {filter, map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,30 +11,14 @@ export class PhotographService {
 
   private EVENT_REF_PATH = 'snapEvents';
 
-  constructor(private db: AngularFireDatabase, private game: GameManagementService) {}
+  constructor(private db: AngularFireDatabase) {}
 
   public getCurrentGameSnappedObservable(): Observable<SnapEvent> {
-    // TODO for debug
-    return interval(600).pipe(
-      map(() => {
-        return {
-          gameId: 'lfkjslf',
-          photographerId: 'lsjf',
-          subjectId: 'dlf',
-          photoUrl: 'assets/camera_kao_ninshiki.png',
-          photoPath: '',
-          createdAt: new Date()
-        };
-      })
-    );
-
-    // const gameId = this.game.currentGame.id;
-    // return this.db.list(`${this.EVENT_REF_PATH}/${gameId}`)
-    //   .valueChanges()
-    //   .pipe(
-    //     map((e: any[]) => e[e.length - 1]),
-    //     map((e: any) => Object.assign({gameId: gameId}, e)),
-    //   );
+    return this.db.list(this.EVENT_REF_PATH)
+      .valueChanges()
+      .pipe(
+        filter((e: any[]) => !!e.length),
+        map((e: any[]) => e[e.length - 1])
+      );
   }
-
 }
