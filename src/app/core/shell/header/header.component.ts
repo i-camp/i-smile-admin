4d7c553ef2325df9eb2bb055f5cd64577d16488f
nakahararuu <repika02@gmail.com>
2018-07-16 16:28:44 +0900

    remove i18n menu on header

--- src/app/core/shell/header/header.component.ts
+++ src/app/core/shell/header/header.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import { I18nService } from '../../i18n.service';
-
 @Component({
   selector: 'sml-header',
   templateUrl: './header.component.html',
@@ -10,24 +8,12 @@ import { I18nService } from '../../i18n.service';
 export class HeaderComponent implements OnInit {
 
   menuHidden = true;
-  constructor(private i18nService: I18nService) { }
+  constructor() { }
 
   ngOnInit() { }
 
   toggleMenu() {
     this.menuHidden = !this.menuHidden;
-  }
-
-  setLanguage(language: string) {
-    this.i18nService.language = language;
-  }
-
-  get currentLanguage(): string {
-    return this.i18nService.language;
-  }
-
-  get languages(): string[] {
-    return this.i18nService.supportedLanguages;
   }
 
 }
