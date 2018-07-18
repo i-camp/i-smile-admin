e4b97a50d6f7cfe7782e29ea3278fc2f1d8be38c
tsukamotota <tsukamotota@istyle.co.jp>
2018-07-18 15:28:11 +0900

    ゲーム時間を10分に設定

--- src/app/core/shell/game-controller/game-controller.component.ts
+++ src/app/core/shell/game-controller/game-controller.component.ts
@@ -30,7 +30,7 @@ export class GameControllerComponent implements OnInit, OnDestroy {
     }));
 
     // TODO should disable link until created Game
-    this.game.createGame({during: 30});
+    this.game.createGame({during: 600});
   }
 
   ngOnDestroy(): void {
